2e7c67caa64f26a4dbfe91603c1543ee96508774
MD JUBER QURAISHI <juberquraishi553@gmail.com>
2026-09-05 12:19:32 +0530

    Delete verified-project directory

--- verified-project/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Required for a lean, self-contained Docker image on Cloud Run.
-  output: "standalone",
-};
-
-module.exports = nextConfig;
